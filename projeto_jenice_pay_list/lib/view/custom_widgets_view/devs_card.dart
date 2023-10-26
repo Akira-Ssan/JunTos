9780537384e2a1aab7af6daa210b7a15143c5787
Flebersom Bezerra <108163867+Akira-Ssan@users.noreply.github.com>
2023-10-26 06:03:14 -0300

    Remove Artifacts devs_card.dart

--- projeto_jenice_pay_list/lib/view/custom_widgets_view/devs_card.dart
+++ projeto_jenice_pay_list/lib/view/custom_widgets_view/devs_card.dart
@@ -24,21 +24,7 @@ class DevsCard extends StatelessWidget {
               title: Text(titulo),
               subtitle: Text(subtitulo),
             ),
-            /*Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                TextButton(
-                  child: const Text('BUY TICKETS'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-                TextButton(
-                  child: const Text('LISTEN'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-              ],
-            ),*/
+           
           ],
         ),
       ),
